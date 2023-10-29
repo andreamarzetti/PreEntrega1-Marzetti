810050f59ac0f6288d682e173533991e76652368
Andrea Marzetti <amarzetti@frba.utn.edu.ar>
2023-10-29 13:02:54 -0300

    primer pre entrega

---
+++ pikiestilo/src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import cart from "../assets/compras.png"
+
+export const CartWidget = () => {
+
+    return ( 
+    <>
+      <img src={cart} alt="Carrito" width="40" /> <span>0</span>
+    </>
+    );
+};
